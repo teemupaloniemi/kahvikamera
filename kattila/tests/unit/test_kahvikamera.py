fcb9ca5b63cf43a5a8035b84a3cccc581d86ceb6
Teemu Paloniemi <tealjapa@student.jyu.fi>
2024-01-18 13:41:06 +0200

    lisätty mse yksikkötestit

--- kattila/tests/unit/test_kahvikamera.py
+++ kattila/tests/unit/test_kahvikamera.py
@@ -1,11 +1,36 @@
 import unittest
+import cv2
+import os
+import kahvikamera
 
 class TestImageDifference(unittest.TestCase):
 
-    def test_eroavatkuvat_eroavat(self):
-        result = 1.0 # TODO ÄLÄ JÄTÄ
-        unexpected = 0.0
-        self.assertNotEqual(result, unexpected)
+    def test_mse_eroavatkuvat_eroavat(self):
+        # Setup
+        path = "/home/kattila/kahvicam/kahvikamera/kattila/tests/testdata/"
+    
+        imgA = cv2.imread(os.path.join(path, 'kahvi40proc.jpg'))
+        imgB = cv2.imread(os.path.join(path, 'kahvi35proc.jpg'))
+        imgA = kahvikamera.cv_preprocess(imgA)
+        imgB = kahvikamera.cv_preprocess(imgB)
+
+        result = kahvikamera.mse(imgA, imgB)
+
+        self.assertNotEqual(result, 0.0)
+
+    def test_mse_samatkuvat_eieroa(self):
+        # Setup
+        path = "/home/kattila/kahvicam/kahvikamera/kattila/tests/testdata/"
+    
+        imgA = cv2.imread(os.path.join(path, 'kahvi40proc.jpg'))
+        imgB = cv2.imread(os.path.join(path, 'kahvi40proc.jpg'))
+        imgA = kahvikamera.cv_preprocess(imgA)
+        imgB = kahvikamera.cv_preprocess(imgB)
+
+        result = kahvikamera.mse(imgA, imgB)
+
+        self.assertAlmostEqual(result, 0.0, 1, "Samat kuvat eivät kuulu erota, mitävittua")
+
 
 if __name__ == '__main__':
     unittest.main()
