dbbcc07c1afd420f8d1e14d8ec9b982ce4f428a5
test <you@example.com>
2024-01-27 21:07:55 +0200

    display endpoint added

--- web/app.py
+++ web/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, Response
-import random
 import time
 
 app = Flask(__name__)
@@ -17,19 +16,55 @@ def poista_vanhat_halukkaat():
             temp.append(h)
     halukkaat = temp
 
+def lisaa_uusi_halukas():
+    global halukkaat
+    timestamp = time.time()
+    poista_vanhat_halukkaat()
+    if len(halukkaat) >= HALUKKAATMAX:
+        halukkaat.pop(0)
+    halukkaat.append(timestamp)
+
 #========= main =========
+
+def halukkaatstr():
+    halukkaatstr = "Halukkaat"
+    halukkaatlkm = len(halukkaat)
+    if   (halukkaatlkm == 3):  halukkaatstr = halukkaatstr + f":{halukkaatlkm}"
+    elif (halukkaatlkm >= HALUKKAATMAX): halukkaatstr = halukkaatstr + f": {halukkaatlkm} (max)"
+    else:                      halukkaatstr = halukkaatstr + f": {halukkaatlkm}"
+    return halukkaatstr
+
+@app.route('/kahvi',  methods=['POST'])
+def home():
+    if len(halukkaat) >= HALUKKAATMAX:
+        halukkaats = halukkaatstr() 
+        return render_template('index.html', halukkaat=halukkaats), 418
+
+    lisaa_uusi_halukas()
+    x = render_template('redirect.html')
+    resp = Response(x)
+    resp.headers.add('Location', 'https://kattila.cafe')
+    return resp
 
 @app.route('/', methods=['GET'])
 def kissa():
-    #poista_vanhat_halukkaat()
-    return render_template('index.html', halukkaat=str(len(halukkaat))+"%")
+    poista_vanhat_halukkaat()
+    halukkaats = halukkaatstr() 
+    return render_template('index.html', halukkaat=halukkaats)
 
 @app.route('/favicon.ico')
 def favicon():
     return app.send_static_file('favicon.ico')
+
+@app.route('/display')
+def toinen_kissa():
+    poista_vanhat_halukkaat()
+    halukkaats = halukkaatstr() 
+    return render_template('display.html', halukkaat=halukkaats, halukkaatlkm=str(len(halukkaat))+"%")
 
 def main():
     app.run(host='127.0.0.1', port=5000, debug=True)
 
 if __name__ == '__main__':
     main()
+
